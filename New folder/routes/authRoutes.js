6224af1573c00f8dc031848ceb9fbcdc5449b95c
Aayush766 <blaze.aayush23@gmail.com>
2025-12-04 13:52:31 +0530

    updatessssss

--- New folder/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginUser } = require('../controllers/authController.js');
-
-router.post('/login', loginUser);
-
-module.exports = router;
--- New folder/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { setInitialPassword } = require('../controllers/userController.js');
-const { protect } = require('../middleware/authMiddleware.js');
-
-
-router.put('/set-password', protect, setInitialPassword);
-
-module.exports = router;
